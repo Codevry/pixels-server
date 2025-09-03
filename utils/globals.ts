2ec921af3fb6b9e6aebd79fc2da633d0db9bdfd5
Saksham <dawnimpulse@gmail.com>
2025-09-03 17:34:24 +0530

    feat(global): config

--- utils/globals.ts
+++ utils/globals.ts
@@ -1,5 +1,7 @@
 import type { DbRedis } from "@/services/dbRedis.ts";
+import type { TypeConfig } from "@/types/typeConfig.ts";
 
 export default class Globals {
     static dbRedis: DbRedis;
+    static config: TypeConfig;
 }
