7c162ab927fe739fa195d764a573a3280fd26698
Saksham <dawnimpulse@gmail.com>
2025-09-04 13:22:17 +0530

    feat(globals): storage

--- utils/globals.ts
+++ utils/globals.ts
@@ -1,7 +1,9 @@
 import type { DbRedis } from "@/services/dbRedis.ts";
 import type { TypeConfig } from "@/types/typeConfig.ts";
+import type { TypeStorage, TypeStorageManager } from "@/types/typeStorage.ts";
 
 export default class Globals {
     static dbRedis: DbRedis;
     static config: TypeConfig;
+    static storage: TypeStorageManager = {} as TypeStorageManager;
 }
