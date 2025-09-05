0c4703601a9966c62875a1daecf2420855ca6056
Saksham <dawnimpulse@gmail.com>
2025-09-05 16:26:19 +0530

    feat(globals): ctrlRedis

--- src/utils/globals.ts
+++ src/utils/globals.ts
@@ -1,9 +1,11 @@
 import type { DbRedis } from "@/services/dbRedis.ts";
 import type { TypeConfig } from "@/types/typeConfig.ts";
 import type { TypeStorage, TypeStorageManager } from "@/types/typeStorage.ts";
+import CtrlRedis from "@/controller/ctrlRedis.ts";
 
 export default class Globals {
     static dbRedis: DbRedis;
     static config: TypeConfig;
     static storage: TypeStorageManager = {} as TypeStorageManager;
+    static ctrlRedis: CtrlRedis = new CtrlRedis();
 }
