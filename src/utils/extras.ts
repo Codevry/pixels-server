57f316274ff16976a950cb43f3442d939907197b
Saksham <dawnimpulse@gmail.com>
2025-09-05 13:27:49 +0530

    feat(extras): imageConversionParams + formatMap

---
+++ src/utils/extras.ts
@@ -0,0 +1,31 @@
+import type { FormatEnum } from "sharp";
+
+// useful for creating image name
+export const imageConversionParams = {
+    height: "h",
+    width: "w",
+    quality: "q",
+    format: "f",
+    blur: "b",
+    greyscale: "g",
+    rotate: "r",
+    flip: "flip",
+    flop: "flop",
+    tint: "t",
+    negate: "n",
+};
+
+// useful for extension of image
+export const formatMap: Record<string, keyof FormatEnum> = {
+    jpg: "jpg",
+    jpeg: "jpeg",
+    png: "png",
+    webp: "webp",
+    gif: "gif",
+    tiff: "tiff",
+    avif: "avif",
+    heif: "heif",
+    jp2: "jp2",
+    jxl: "jxl",
+    raw: "raw",
+};
