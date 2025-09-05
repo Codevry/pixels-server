04474594442d8dedcbea63f765091668a23573e2
Saksham <dawnimpulse@gmail.com>
2025-09-05 16:26:44 +0530

    feat(ctrl): redis - ifImageConverted & removeImageConverted

---
+++ src/controller/ctrlRedis.ts
@@ -0,0 +1,18 @@
+import Globals from "@/utils/globals.ts";
+
+export default class CtrlRedis {
+    /**
+     * check if converted image exists in cache
+     */
+    async ifImageConverted(image: string): Promise<Boolean> {
+        return Globals.dbRedis.exists(`processed:image:${image}`);
+    }
+
+    /**
+     * remove the image path from the cache
+     * @param image
+     */
+    async removeImageConverted(image: string): Promise<boolean> {
+        return (await Globals.dbRedis.delete(`processed:image:${image}`)) > 0;
+    }
+}
