cf35aaaf61abb0872e9d4a68643a4167e03811b3
Saksham <dawnimpulse@gmail.com>
2025-09-05 20:33:52 +0530

    feat(ctrl): redis - rename methods

--- src/controller/ctrlRedis.ts
+++ src/controller/ctrlRedis.ts
@@ -4,7 +4,7 @@ export default class CtrlRedis {
     /**
      * check if converted image exists in cache
      */
-    async ifImageConverted(image: string): Promise<Boolean> {
+    async findImageRef(image: string): Promise<Boolean> {
         return Globals.dbRedis.exists(`processed:image:${image}`);
     }
 
@@ -12,7 +12,7 @@ export default class CtrlRedis {
      * remove the image path from the cache
      * @param image
      */
-    async removeImageConverted(image: string): Promise<boolean> {
+    async removeImageRef(image: string): Promise<boolean> {
         return (await Globals.dbRedis.delete(`processed:image:${image}`)) > 0;
     }
 
@@ -20,7 +20,7 @@ export default class CtrlRedis {
      * save converted image in cache
      * @param image
      */
-    async saveImageConverted(image: string): Promise<boolean> {
+    async saveImageRef(image: string): Promise<boolean> {
         return (
             (await Globals.dbRedis.create(
                 `processed:image:${image}`,
