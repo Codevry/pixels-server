5af3738aa88d928d9ba93211ddfd49ac457b9c04
Saksham <dawnimpulse@gmail.com>
2025-09-05 14:09:46 +0530

    feat(type): image - conversionParams

---
+++ src/types/typeImage.ts
@@ -0,0 +1,12 @@
+export type TypeImageConversionParams = {
+    width: number;
+    height: number;
+    quality: number;
+    format: string;
+    blur: number;
+    greyscale: boolean;
+    rotate: number;
+    flip: boolean;
+    flop: boolean;
+    tint: string;
+};
