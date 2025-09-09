bffb886a67dd424ad68c7515c21ed55d91fd9b67
Saksham <dawnimpulse@gmail.com>
2025-09-09 14:07:10 +0530

    feat(ctrl): batch - using for await & saving progress in redis

--- src/controller/ctrlBatch.ts
+++ src/controller/ctrlBatch.ts
@@ -1,26 +1,31 @@
 import { ErrorObject } from "@/utils/errorObject.ts";
 import Globals from "@/utils/globals.ts";
 import type { TypeImageConversionParams } from "@/types/typeImage.ts";
-import CtrlImage from "@/controller/ctrlImage.ts";
 import {
     createNameFromParams,
     validateImageExtension,
     validateQueryParams,
 } from "@/utils/functions.ts";
+import Silent from "@/utils/silent.ts";
 
 export default class CtrlBatch {
     /**
-     * Performs batch transformation and upload of images from a specified directory in a storage.
+     * Performs batch transformation and upload of images from a specified directory in storage.
+     * Processes each image in the directory applying the specified transformations and tracks
+     * progress using Redis with the provided token.
+     *
+     * @param {string} token - Unique identifier for tracking the batch processing progress.
      * @param {string} storageName - The name of the storage configuration to use (e.g., 's3_main').
      * @param {string} directoryPath - The directory path within the storage to list files from.
      * @param {TypeImageConversionParams} transformations - A JSON object of transformations to apply.
-     * @returns {Promise<{ done: number; pending: number; errors: any[] }>} An object with counts of done, pending, and errors.
+     * @returns {Promise<void>} This method doesn't return a value directly. Progress can be tracked using the token in Redis.
      */
     public async batchTransformAndUpload(
+        token: string,
         storageName: string,
         directoryPath: string,
         transformations: Record<string, any>
-    ): Promise<{ done: number; pending: number; errors: any[] }> {
+    ): Promise<void> {
         // validate transformation
         const validateTransformations = validateQueryParams(transformations);
 
@@ -53,59 +58,67 @@ export default class CtrlBatch {
         let done = 0;
         let pending = files.length;
         const errors: any[] = [];
-        const ctrlImage = new CtrlImage();
+
+        // save in redis
+        Silent(
+            "createBatchProgress",
+            Globals.ctrlRedis.saveProgress(token, { done, pending, errors })
+        );
 
         // apply operation on each image
-        const BATCH_SIZE = 5;
-        for (let i = 0; i < files.length; i += BATCH_SIZE) {
-            const batch = files.slice(i, i + BATCH_SIZE);
-            const batchPromises = batch.map(async (filePath) => {
-                try {
-                    // Extract file extension and base name from the path
-                    const ext = filePath.split(".").pop() || "";
-                    const originalName =
-                        filePath.split("/").pop()?.split(".")[0] || "";
+        for await (const filePath of files) {
+            try {
+                // Extract file extension and base name from the path
+                const ext = filePath.split(".").pop() || "";
+                const originalName =
+                    filePath.split("/").pop()?.split(".")[0] || "";
 
-                    // make sure ext is present
-                    if (!ext)
-                        throw new ErrorObject(400, "File extension is missing");
+                // make sure ext is present
+                if (!ext)
+                    throw new ErrorObject(400, "File extension is missing");
 
-                    // validate original extension
-                    const originalExtension = validateImageExtension(ext)!;
+                // validate original extension
+                const originalExtension = validateImageExtension(ext)!;
 
-                    // validate new extension (if present)
-                    const newExtension = validateImageExtension(
-                        transformations.format
-                    );
+                // validate new extension (if present)
+                const newExtension = validateImageExtension(
+                    transformations.format
+                );
 
-                    // New name (unique name) for parse operations
-                    const parsedName = createNameFromParams(
-                        originalName,
-                        newExtension || originalExtension,
-                        transformations
-                    );
+                // New name (unique name) for parse operations
+                const parsedName = createNameFromParams(
+                    originalName,
+                    newExtension || originalExtension,
+                    transformations
+                );
 
-                    // process the image
-                    await ctrlImage.processImage(
-                        storageName,
-                        filePath,
-                        parsedName,
-                        transformations,
-                        originalExtension,
-                        newExtension || originalExtension
-                    );
-                    done++;
-                    pending--;
-                    console.log(`Processed ${done}/${files.length} files`);
-                } catch (error: any) {
-                    console.error(`Error processing file ${filePath}:`, error);
-                    errors.push({ filePath, error: error.message || error });
-                    pending--;
-                }
-            });
-            await Promise.all(batchPromises);
+                // process the image
+                await Globals.ctrlImage.processImage(
+                    storageName,
+                    filePath,
+                    parsedName,
+                    transformations,
+                    originalExtension,
+                    newExtension || originalExtension
+                );
+                done++;
+                pending--;
+                console.log(`Processed ${done}/${files.length} files`);
+            } catch (error: any) {
+                console.error(`Error processing file ${filePath}:`, error);
+                errors.push({ filePath, error: error.message || error });
+                pending--;
+            } finally {
+                // save in redis
+                Silent(
+                    "updateBatchProgress",
+                    Globals.ctrlRedis.saveProgress(token, {
+                        done,
+                        pending,
+                        errors,
+                    })
+                );
+            }
         }
-
-        return { done, pending, errors };
     }
 }
