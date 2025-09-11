af52c1e2d8d36a9e2b199df3d0b208b11d82b239
Saksham <dawnimpulse@gmail.com>
2025-09-11 12:01:38 +0530

    feat(globals): ctrlConfig

--- src/utils/globals.ts
+++ src/utils/globals.ts
@@ -4,6 +4,7 @@ import type { TypeStorageManager } from "@/types/typeStorage.ts";
 import CtrlRedis from "@/controller/ctrlRedis.ts";
 import CtrlBatch from "@/controller/ctrlBatch.ts";
 import CtrlImage from "@/controller/ctrlImage.ts";
+import { CtrlConfig } from "@/controller/ctrlConfig.ts";
 
 export default class Globals {
     static dbRedis: DbRedis;
@@ -12,4 +13,5 @@ export default class Globals {
     static ctrlRedis: CtrlRedis = new CtrlRedis();
     static ctrlBatch = new CtrlBatch();
     static ctrlImage = new CtrlImage();
+    static ctrlConfig = new CtrlConfig();
 }
